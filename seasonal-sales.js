f26bcab756c637e64e0cb2121e72e4fb3ff75ebd
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-06 09:25:09 -0600

    deleted instructions.  Added section outlining overall structure

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,36 +1,18 @@
-// Your job is to build a web page that lists all of the products,
-// the name of the department it's in, and the price.
-// Additionally, put a <select> element at the top of the page
-// that contains all possible values of the season_discount key
-// in the categories file.
-// As soon as you select one of the seasons,
-// all prices on the page should immediately be discounted
-// by the corresponding percentage.
-
-// For example, when Spring is chosen,
-// all products in the corresponding Household category
-// should have their prices updated
-// with a 15% discount off the base price.
-
-// The two JSON representations above should be in two files:
-// products.json, and categories.json.
-// You should load both file via XHRs and store the contents
-// in two different JavaScript variables in your code.
-
 ////////////////////////////////////////////////////////////
-/////////               Objectives             /////////////
+/////////               Structure              /////////////
 ////////////////////////////////////////////////////////////
 
-// Initial stuff to play around with:
-  // JSON
-    // Importing json objects using XML HTTP Request
-    // Parsing and storing data
-  // HTML
-    // Page Layout
-    // Display product info in table?
-  // Javascript
-    // How do I immediately update info upon selection of season?
-      // Event listener that listens for an input from that
+// Open connection to products JSON
+// Upon load, run parseProducts
+  // Parse the JSON object and trigger load of categories
+// Upon load, run generateHTML
+  // Parse categories JSON object and generate table
+
+// Event Listener
+// Listen for change of select element
+  // First, reset prices
+  // Then, use data from productsJSON and categoriesJSON to
+  // apply discounts
 
 // variables for JSON objects
 var productsJSON;
